5c9ede156472ed9f5bdc6af053b8d16cd6d13fc5
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2022-07-02 15:57:29 +0300

    lab1

---
+++ CudaRuntime1/CudaRuntime1/kernel.cu
@@ -0,0 +1,25 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+__global__ void ab(int a, int b, int* c) {
+	*c = a + b;
+}
+__global__ void Enter()
+{
+	printf("Enter 2 numbers\n");
+}
+int main() {
+	int a, b;
+	Enter << <1, 1 >> > ();
+	cin >> a >> b;
+	int c;
+	int* c1;
+	cudaMalloc((void**)&c1, sizeof(int));
+	ab << <1, 1 >> > (a, b, c1);
+	cudaMemcpy(&c, c1, sizeof(int), cudaMemcpyKind::cudaMemcpyDeviceToHost);
+	cout << c;
+	return 0;
+}
